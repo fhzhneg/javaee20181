d8aff8795d3ec33310522c55a4e0dc630b73e8c0
fhzhneg <fhzheng304@qq.com>
2018-12-21 15:56:49 +0800

    20181221 - v3.0 give memento pattern

--- src/main/java/memento/Main.java
+++ src/main/java/memento/Main.java
@@ -1,32 +1,41 @@
 package memento;
 
+import memento.game.Gamer;
+import memento.game.Memento;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		//初态
 		Gamer gamer = new Gamer(100);
+		//快照一下
 		Memento memento = gamer.createMemento();
+		
+		//游戏开始
 		for (int i = 0; i < 100; i++) {
-			System.out.println("==== "+i);
-			System.out.println("当前状态："+gamer);
-		}
-		
-		gamer.bet();
-		
-		System.out.println("所有金币为：" + gamer.getMoney()+"个。");
-		if (gamer.getMoney() > memento.getMoney()) {
-			System.out.println("     (所持金钱增加了许多，因此保存游戏当前的状态)");
-			memento = gamer.createMemento();
-		} else if (gamer.getMoney() < memento.getMoney()) {
-			System.out.println("     (所持金钱减少了许多，因此将游戏恢复至以前的状态)");
-			gamer.restoreMemento(memento);
-		}
-		
-		try {
-			Thread.sleep(1000);
-		} catch (Exception e) {
-		}
-		System.out.println();
-	}
+			System.out.println("==== "+i);			//游戏次数
+			System.out.println("当前状态："+gamer);	//当前态
 
+			gamer.bet();							//进行游戏
+			
+			System.out.println("所有金币为：" + gamer.getMoney()+"个。");
+			
+			//如何处理Memento，即使用记忆
+			if (gamer.getMoney() > memento.getMoney()) {			//优则存
+				System.out.println("     (所持金钱增加了许多，因此保存游戏当前的状态)");
+				memento = gamer.createMemento();
+			} else if (gamer.getMoney() < memento.getMoney()) {		//损则复原
+				System.out.println("     (所持金钱减少了许多，因此将游戏恢复至以前的状态)");
+				gamer.restoreMemento(memento);
+			}
+			
+			//系统等待
+			try {
+				Thread.sleep(100);
+			} catch (Exception e) {
+			}
+			System.out.println();
+		}
+	}
 }
--- src/main/java/memento/game/Gamer.java
+++ src/main/java/memento/game/Gamer.java
@@ -1,4 +1,4 @@
-package memento;
+package memento.game;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -7,65 +7,97 @@ import java.util.Random;
 
 public class Gamer {
 
-	private int money;
-	private List fruits = new ArrayList();
-	private Random random = new Random();
+	private int money;							//金币数
+	private List fruits = new ArrayList();		//水果
 	
+	private Random random = new Random();		//随机数生成器
+	
+	/**
+	 * 水果种类
+	 */
 	private static String[] fruitsname = {
 		"苹果","葡萄","香蕉","李子","桃子","菠萝",	
 	};
 	
+	/**
+	 * 构造函数
+	 * @param money
+	 */
 	public Gamer(int money) {
 		this.money = money;
 	} 
 	
+	/**
+	 * 获取金币
+	 * @return 金币
+	 */
 	public int getMoney() {
 		return money;
 	}
 	
+	/**
+	 * 进行投掷骰子的游戏
+	 */
 	public void bet() {
-		int dice = random.nextInt(6)+1;
-		if(dice == 1) {
+		int dice = random.nextInt(6)+1;					//投出
+		if(dice == 1) {									//赢了，增加金币
 			money += 100;
 			System.out.println("所持金币增加了。");
-		} else if (dice == 2) {
+		} else if (dice == 2) {							//输出，金币减少
 			money /=2;
 			System.out.println("所持金币减半了。");
-		} else if (dice == 6) {
+		} else if (dice == 6) {							//获得水果，运气
 			String f = getFruit();
 			System.out.println("获得了水果("+f+")。");
 			fruits.add(f);
-		} else {
+		} else {										//3,4,5啥也没发生
 			System.out.println("什么都没有发生。");
 		}
 	}
 	
+	/**
+	 * 拍摄快照
+	 * @return 快照
+	 */
 	public Memento createMemento() {
-		Memento m = new Memento();
+		
+		Memento m = new Memento(money);					//金币数记录
 		Iterator it = fruits.iterator();
-		while (it.hasNext()) {
+		while (it.hasNext()) {							//水果记录
 			String f = (String)it.next();
-			if (f.startsWith("好吃的")) {
+			if (f.startsWith("好吃的")) {					//只保存好吃的水果
 				m.addFruit(f);
 			}
 		}
 		return m;
 	}
 	
+	/**
+	 * 快照恢复，即撤销
+	 * @param memento 传入快照
+	 */
 	public void restoreMemento(Memento memento) {
 		this.money = memento.money;
 		this.fruits = memento.getFruits();
 	}
 	
+	/**
+	 * 用字符串表示主人公当状态
+	 * 即输出金币数和水果列表
+	 */
 	public String toString() {
 		return "[money = "+money+", fruits = "+fruits+"]";
 	}
 	
+	/**
+	 * 获得一个水果
+	 * @return 好吃的XX[水果]
+	 */
 	private String getFruit() {
 		String prefix = "";
 		if (random.nextBoolean()) {
 			prefix = "好吃的";
 		}
-		return prefix = fruitsname[random.nextInt(fruitsname.length)];
+		return prefix+fruitsname[random.nextInt(fruitsname.length)];
 	}
 }
