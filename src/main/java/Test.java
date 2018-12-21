db1282e01a81075b1bfe6ba22329f229598dbe07
fhzhneg <fhzheng304@qq.com>
2018-12-21 11:08:56 +0800

    4 modity Test.java--->中文输出

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,14 +1,13 @@
 public class Test {
 
 	public static void main(String[] args) {
-		
+
 		display("fhzheng");
 	}
-	
+
 	public static void display(String name) {
-		
-		System.out.println(name + "Helloworld");
+
+		System.out.println(name + ",你好，世界欢迎您加入程序员的队伍中来....");
 	}
-	
-	
+
 }
